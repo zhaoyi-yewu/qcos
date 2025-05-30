a2a799dee702b29f1a893f4b7c6a440bff8bec9f
jidalong <jidalong_yewu@cmss.chinamobile.com>
2025-05-30 11:34:52 +0800

    modify frame
    Description:

--- src/compiler/__init__.py
+++ src/compiler/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------
-# Copyright© 2014-2025 China Mobile (SuZhou) Software Technology Co.,Ltd.
+# Copyright© 2025 China Mobile (SuZhou) Software Technology Co.,Ltd.
 #
 # qcos is licensed under Mulan PSL v2.
 # You can use this software according to the terms and conditions
@@ -12,6 +12,4 @@
 # EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
-#
-# Create by Ji Dalong at 2025-05
 # ----------------------------------------------------------------------
