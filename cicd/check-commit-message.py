631f487fb99bb044bdfdaf11d0bddcfdcfe85a73
zhaoyi_yewu <zhaoyi_yewu@cmss.chinamobile.com>
2026-01-16 09:22:36 +0800

    Added new gate check to avoid duplicated commits
    Description:
    1. 增加对重复提交commit的门禁检查

--- cicd/check-commit-message.py
+++ cicd/check-commit-message.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------
-# Copyright© 2024-2025 China Mobile (SuZhou) Software Technology Co.,Ltd.
+# Copyright© 2024-2026 China Mobile (SuZhou) Software Technology Co.,Ltd.
 #
 # qcos is licensed under Mulan PSL v2.
 # You can use this software according to the terms and conditions
@@ -24,6 +24,7 @@ import re
 import subprocess
 import sys
 
+
 def get_commit_messages():
     """Get commit messages."""
     # check gitlab ci env variables
@@ -45,16 +46,53 @@ def get_commit_messages():
             first_line = msg.split('\n')[0].strip()
             return first_line
         except subprocess.CalledProcessError as e:
-            sys.stderr.write(f"ERROR: Failed to run git command: {e}", file=sys.stderr)
+            sys.stderr.write(f"ERROR: Failed to run git command: {e}",
+                             file=sys.stderr)
             sys.exit(1)
         except FileNotFoundError:
-            sys.stderr.write("ERROR: Failed to find git command", file=sys.stderr)
+            sys.stderr.write("ERROR: Failed to find git command",
+                             file=sys.stderr)
             sys.exit(1)
+
+
+def get_commit_messages_summary(count=1):
+    """Get the last n commit message summaries.
+
+    Args:
+        count (int): Number of commits
+
+    Returns:
+        commit message list
+    """
+    # use git command to get the last n commit message summaries
+    commit_message_list = []
+    try:
+        result = subprocess.run(
+            ['git', 'log', f'-{count}', '--pretty=format:%h %s'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        msg = result.stdout.strip()
+        _commit_message_list = msg.split('\n')
+        for line in _commit_message_list:
+            commit_message_list.append(tuple(line.split(" ", maxsplit=1)))
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write(f"ERROR: Failed to run git command: {e}",
+                         file=sys.stderr)
+        sys.exit(1)
+    except FileNotFoundError:
+        sys.stderr.write("ERROR: Failed to find git command",
+                         file=sys.stderr)
+        sys.exit(1)
+    return commit_message_list
+
 
 def is_english_string(s):
     """Check if is english."""
     ascii_pattern = re.compile(r'^[\x20-\x7E]+$')
     return ascii_pattern.match(s) is not None
+
 
 def main():
     print("Checking commit messages")
@@ -68,15 +106,35 @@ def main():
 
     # check leading characters
     if first_line != first_line.lstrip():
-        sys.stderr.write("ERROR: Leading characters of commit message cannot be blank\n")
+        sys.stderr.write(
+            "ERROR: Leading characters of commit message cannot be blank\n")
         sys.exit(1)
 
     # check if is english
     if not is_english_string(first_line):
-        sys.stderr.write("ERROR: Commit message summary includes non-English " \
-            "characters, which is not allowed. Only English letters are allowed\n")
+        sys.stderr.write("ERROR: Commit message summary includes "
+                         "non-English characters, which is not allowed. "
+                         "Only English letters are allowed\n")
         sys.exit(1)
+
+    # check duplicated commit messages
+    commit_message_list = get_commit_messages_summary(count=10)
+    last_commit_summary = None
+    last_commit_hash = None
+    for commit_hash, commit_summary in commit_message_list:
+        commit_summary = commit_summary.strip()
+        if commit_summary == last_commit_summary:
+            sys.stderr.write("ERROR: commit message summary "
+                             f"(hash: {last_commit_hash}) is identical "
+                             f"to another commit (hash: {commit_hash}). "
+                             "Please squash them into one commit\n")
+            sys.exit(1)
+        if not last_commit_summary:
+            last_commit_summary = commit_summary
+            last_commit_hash = commit_hash
+
     print("[SUCCESS]")
+
 
 if __name__ == "__main__":
     main()
