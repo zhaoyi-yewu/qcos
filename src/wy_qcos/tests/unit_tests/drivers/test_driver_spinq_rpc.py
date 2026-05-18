1afaa138d6bec059279bc78eea7fbb307e2943ce
zhaoyi_yewu <zhaoyi_yewu@cmss.chinamobile.com>
2026-05-18 10:01:00 +0800

    Update driver unit tests for remote devices
    Description:
    1. 更新远程设备驱动单元测试

--- src/wy_qcos/tests/unit_tests/drivers/test_driver_spinq_rpc.py
+++ src/wy_qcos/tests/unit_tests/drivers/test_driver_spinq_rpc.py
@@ -20,7 +20,9 @@
 from wy_qcos.common.config import Config
 from wy_qcos.common.library import Library
 
-org_path = Library.set_driver_venv_path("DriverSpinQRpc", Config.VENV_DIR)
+org_path = Library.set_driver_venv_path(
+    "DriverSpinQRpc", Config.DEFAULT.VENV_DIR
+)
 
 import json
 import pytest
