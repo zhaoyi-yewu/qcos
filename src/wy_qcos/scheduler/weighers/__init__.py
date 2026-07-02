65ec126481fc9e5b092503a11070a93e8d0fc77d
zhaoyi_yewu <zhaoyi_yewu@cmss.chinamobile.com>
2026-07-02 17:11:05 +0800

    Add scheduler weighers module
    Description:
    1. 新增调度器权重器模块，包含权重器基类及DeviceLoad设备负载和AvgExecTime平均执行时间两种权重器实现

---
+++ src/wy_qcos/scheduler/weighers/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Copyright© 2024-2026 China Mobile (SuZhou) Software Technology Co.,Ltd.
+#
+# qcos is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions
+# of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#         http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS,
+#     WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ----------------------------------------------------------------------
+
+from .base import BaseWeigher, BaseWeightHandler, WeightedDevice
+from .device_load import DeviceLoadWeigher
+from .exec_time import AvgExecTimeWeigher
+
+# Default weigher order
+DEFAULT_WEIGHERS = [
+    DeviceLoadWeigher,
+    AvgExecTimeWeigher,
+]
+
+# Weigher registry for auto-discovery
+WEIGHER_REGISTRY = {
+    "DeviceLoadWeigher": DeviceLoadWeigher,
+    "AvgExecTimeWeigher": AvgExecTimeWeigher,
+}
